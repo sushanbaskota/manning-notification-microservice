fe3645ccc31a6308330068e26ef340d5e72335a8
sushanbaskota <sushan.baskota13@gmail.com>
2024-04-27 22:18:13 -0500

    reformat

--- NotificationDiscoveryServer/src/main/java/com/baskota/notificationdiscoveryserver/NotificationDiscoveryServerApplication.java
+++ NotificationDiscoveryServer/src/main/java/com/baskota/notificationdiscoveryserver/NotificationDiscoveryServerApplication.java
@@ -11,5 +11,4 @@ public class NotificationDiscoveryServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationDiscoveryServerApplication.class, args);
     }
-
 }
